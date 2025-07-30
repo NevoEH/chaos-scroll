9fb693d3fee96f07f918f2a3c0d5ca1637611ce7
NevoEH <nevo037@gmail.com>
2025-07-30 18:33:28 +0300

    added requirements.txt

--- api/handler.py
+++ api/handler.py
@@ -1,10 +1,9 @@
-# api/handler.py
 from flask import Flask, jsonify
 import random
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api/handler')
 def chaos_scroll():
     try:
         success = random.choices(['fail', 'pass'], weights=[40, 60])[0]
@@ -13,6 +12,7 @@ def chaos_scroll():
         else:
             stat_change = random.randint(-5, 5)
             result = f"Chaos Scroll has passed and your item got a {stat_change:+d} stats !"
+
         return jsonify({"result": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
