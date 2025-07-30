db51fad8caf40449b02476d1324dfa5bb26f16a9
NevoEH <nevo037@gmail.com>
2025-07-30 17:18:49 +0300

    chaos scroll simulator

---
+++ api/chaos.py
@@ -0,0 +1,12 @@
+# api/chaos.py
+import random
+
+def handler(request, response):
+    success = random.choices(['fail', 'pass'], weights=[40, 60])[0]
+    if success == 'fail':
+        result = "Chaos Scroll has failed. No changes made, an upgrade slot have been consumed."
+    else:
+        stat_change = random.randint(-5, 5)
+        result = f"Chaos Scroll has passed and your item got a {stat_change:+d} stats !"
+
+    return response.json({"result": result})
